ea38412ae406c5e99e0eef2a12954fc37a884e48
chenjun <chenjun@orvibo.com>
2022-12-02 16:55:44 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Counter from './components/counter';
+import { Routes, Route, Link } from 'react-router-dom'
+import GetManager from './components/get_manager';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />      
+        <div className="column column1">
+          <span>
+          <Link to="/">Index</Link> |{' '}
+            <Link to="/counter">Counter</Link> |{' '}
+            <Link to="/getApi">Get Data</Link>
+          </span>
+        </div>
+        <Routes> 
+          <Route path="/" element />
+          <Route path="/counter" element={<Counter value={0} />} />
+          <Route path="/getApi" element={<GetManager />} />
+        </Routes>
       </header>
     </div>
   );
